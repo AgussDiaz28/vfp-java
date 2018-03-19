be3ef7d37258107084fcdd9db1d1323dc6e7f0f0
agussdiaz28 <agussdiaz28@gmail.com>
2018-03-19 13:02:07 -0300

    funcionalidad de sincronizacion de productos

--- src/lectorbdf/MySql.java
+++ src/lectorbdf/MySql.java
@@ -2,6 +2,8 @@ package lectorbdf;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
 
 public class MySql {
@@ -9,30 +11,57 @@ public class MySql {
 	public Connection Connect;
 
 	public MySql() {
+		try {
+			this.getConnection();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}	
+	}
+	
+	public void addProductRecord(String nombre_producto,float categoria_id,float precio_unitario, double maxirest_id) throws Exception {
 		
+		Statement statement = null;
+		
+		try {
+			nombre_producto = nombre_producto.trim();
+			statement = this.Connect.createStatement();
+			String select = ("SELECT precio_unitario FROM producto where maxirest_id = " + (int) maxirest_id );
+			String insert = ("INSERT INTO producto (nombre,categoria_id,precio_unitario,maxirest_id) VALUES ('"+nombre_producto+"',"+ categoria_id +","+ precio_unitario +","+ maxirest_id+")");
+			statement.execute(select);
+			ResultSet rs = statement.getResultSet();
+			if (!rs.next()) { //if i couldn't find the product in the data base i insert this new product
+				statement.execute(insert); 
+			}else {
+				float valor_db = (float) rs.getInt(1);
+				if	( valor_db != precio_unitario ) {
+					String update = ("UPDATE producto set precio_unitario = " + precio_unitario + "where maxirest_id = " + (int) maxirest_id );
+					statement.execute(update);
+				}
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	public Connection getConnection() throws Exception{
 		
         try{ 
-           String url = "jdbc:mysql://localhost:8889/comandas?";
+           String url = "jdbc:mysql://localhost/comandas?";
            String username = "user=root&";
-           String password = "password=root";
- 
-           System.out.println("Loading driver...");
+           String password = "password=mysqlpass&autoReconnect=true&useSSL=false";
 
            try {
                Class.forName("com.mysql.jdbc.Driver");
-               System.out.println("Driver loaded!");
            } catch (ClassNotFoundException e) {
                throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-           }
-          
-           System.out.println("Connecting database...");
-           try (Connection connection = DriverManager.getConnection(url + username + password)) {
-               System.out.println("Database connected!");
+           }           
+           
+           try {
+               this.Connect = DriverManager.getConnection(url + username + password);
            } catch (SQLException e) {
-	        	   System.out.println("SQLException: " + e.getMessage());
                throw new IllegalStateException("Cannot connect the database!", e);
            }
 
--- src/lectorbdf/VfpMannager.java
+++ src/lectorbdf/VfpMannager.java
@@ -1,62 +1,61 @@
 package lectorbdf;
 
 import java.io.*;
-import java.sql.Connection;
-
 import com.linuxense.javadbf.*;
 
 public class VfpMannager {
 	
-	protected Connection Connect;
+	public static final String VFP_FILE_PATH = "/home/superq/Escritorio/DATOS/mxart.dbf";
 	
 	public VfpMannager() {
 		
+	}
+	
+	public static DBFReader get_file_data() {
+		
+		DBFReader reader = null;
+		
+		File file = new File(VFP_FILE_PATH);
+		
+		try {
+			reader = new DBFReader(new FileInputStream(file));
+		} catch (DBFException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // create a DBFReader object
+		
+		return reader;
 	}
 
 	public static void main(String args[]) {
 		
 		MySql mysql = new MySql();
-		try {
-			mysql.Connect = mysql.getConnection();
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
 
-		DBFReader reader = null;
+		Object[] rowObjects;	 // Now, lets us start reading the rows
 		
+		DBFReader reader = VfpMannager.get_file_data();
+	
 		try {
-			
-			File file = new File("/tmp/mxart.dbf");
-			
-			reader = new DBFReader(new FileInputStream(file)); // create a DBFReader object
-
-			int numberOfFields = reader.getFieldCount(); // get the field count if you want for some reasons like the following
-
-			for (int i = 0; i < numberOfFields; i++) {
-
-				DBFField field = reader.getField(i);
-				System.out.println(field.getName());
-			}
-
-			Object[] rowObjects;	 // Now, lets us start reading the rows
-			
-			while ((rowObjects = reader.nextRecord()) != null) {
-					System.out.println(rowObjects[0]); //Codigo
-					System.out.println(rowObjects[2]); //Nombre
+			while ((rowObjects = reader.nextRecord()) != null) {				
+				try {
+					String nombre_producto = (String) rowObjects[2];
+					float categoria_id = (float) 6; //default category
+					float precio_unitario = (float) 0;
 					if	(rowObjects[12] != null) {
-						System.out.println(rowObjects[12]); //Precio
-					}else {
-						System.out.println(0); //Precio
+						precio_unitario = (float) rowObjects[12]; 
 					}
-					
+					double maxirest_id = (double) rowObjects[0];
+					mysql.addProductRecord(nombre_producto,categoria_id,precio_unitario,maxirest_id);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		
-
 		} catch (DBFException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		}			
 	}
 }
 
